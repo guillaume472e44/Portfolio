f48f209c5775e9e289dbb3466529b883bdbbf570
Guillaume Duval <guillaume.duval-dev@orange.fr>
2026-07-28 15:37:19 +0200

    Intégration footer

--- scripts/animations/onload.js
+++ scripts/animations/onload.js
@@ -4,4 +4,12 @@ window.addEventListener("load", () => {
   document
     .querySelectorAll(".section-anchor")
     .forEach((anchor) => anchor.classList.remove("hide"));
+  document.querySelector(".footer__silhouettes").classList.remove("hide");
+  document
+    .querySelector(".Thx1138-silhouette")
+    .addEventListener("transitionend", startAnimations);
 });
+
+function startAnimations() {
+  document.querySelector(".caveman-silhouette").classList.add("animate");
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,2 @@
 export * from "./animations/onload.js"
+export * from "./animations/anchorLinks.js"
