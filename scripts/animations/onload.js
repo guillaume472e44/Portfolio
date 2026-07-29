b904a4e9809541ad9f2d8f4dfd78e357b85cd2f0
Guillaume Duval <guillaume.duval-dev@orange.fr>
2026-07-29 11:38:59 +0200

    Intégration zone liens réseaux sociaux

--- scripts/animations/onload.js
+++ scripts/animations/onload.js
@@ -5,12 +5,18 @@ window.addEventListener("load", () => {
     .querySelectorAll(".section-anchor")
     .forEach((anchor) => anchor.classList.remove("hide"));
   document.querySelector(".footer__silhouettes").classList.remove("hide");
+  document.querySelector(".screen").classList.remove("hide");
 
-  const screen = document.querySelector(".screen");
-  screen.classList.remove("hide");
-  screen.addEventListener("transitionend", startAnimations);
+  const socialLinks = document.querySelector(".social-links");
+  socialLinks.classList.remove("hide");
+  socialLinks.addEventListener("transitionend", startAnimations);
+  // const screen = document.querySelector(".screen");
+  // screen.classList.remove("hide");
+  // screen.addEventListener("transitionend", startAnimations);
 });
 
 function startAnimations() {
   document.querySelector(".caveman-silhouette").classList.add("animate");
+  document.querySelector(".social-links__anchor").classList.add("animate");
+
 }
